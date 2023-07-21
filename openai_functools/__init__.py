d400f09ae8296c70147ae2c3009885348032f4c5
marcvanduyn <marcvanduyn1@gmail.com>
2023-07-21 16:53:04 +0200

    Export FunctionOrchestrator class

--- openai_functools/__init__.py
+++ openai_functools/__init__.py
@@ -1,6 +1,8 @@
 from .metadata_generator import function_metadata_decorator, extract_function_metadata
+from .functions_orchestrator import FunctionsOrchestrator
 
 __all__ = [
     "function_metadata_decorator",
-    "extract_function_metadata"
+    "extract_function_metadata",
+    "FunctionsOrchestrator",
 ]
