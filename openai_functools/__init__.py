e32566e16dd0209f20ace077109dd93177c84cff
marcvanduyn <marcvanduyn1@gmail.com>
2023-07-21 15:26:06 +0200

    Add exports

--- openai_functools/__init__.py
+++ openai_functools/__init__.py
@@ -1,6 +1,9 @@
-from .metadata_generator import function_metadata_decorator, extract_function_metadata
+from .metadata_generator import function_metadata_decorator, \
+    extract_function_metadata
+from .utils import FunctionSpec
 
 __all__ = [
     "function_metadata_decorator",
-    "extract_function_metadata"
+    "extract_function_metadata",
+    "FunctionSpec"
 ]
---
+++ openai_functools/utils/__init__.py
@@ -0,0 +1,3 @@
+from .openai_service import FunctionSpec
+
+__all__ = ["FunctionSpec"]
