3d85dee5cda76d2cc09c4021d0a05fd97991ba87
Jakob Serlier <37184788+Jakob-98@users.noreply.github.com>
2023-07-21 13:35:34 +0200

    set up project

---
+++ scripts/run_static_analysis.ps1
@@ -0,0 +1,14 @@
+# run-black-and-flake8.ps1
+
+# Ensure script stops on first error
+$ErrorActionPreference = "Stop"
+
+# Run black for automatic code formatting
+Write-Output "Running Black for automatic code formatting..."
+poetry run black .
+
+# Run flake8 for linting
+Write-Output "Running Flake8 for linting..."
+poetry run flake8 .
+
+Write-Output "Static analysis completed successfully!"
