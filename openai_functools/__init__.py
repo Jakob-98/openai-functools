dc5be15647e202257c120aa9fafcb320d22850ac
marcvanduyn <marcvanduyn1@gmail.com>
2023-07-24 11:59:14 +0200

    Export FunctionSpec

--- openai_functools/__init__.py
+++ openai_functools/__init__.py
@@ -1,9 +1,11 @@
 from .functions_orchestrator import FunctionsOrchestrator
-from .metadata_generator import (extract_openai_function_metadata,
-                                 openai_function)
+from .metadata_generator import extract_openai_function_metadata, \
+    openai_function
+from .function_spec import FunctionSpec
 
 __all__ = [
     "openai_function",
     "extract_openai_function_metadata",
     "FunctionsOrchestrator",
+    "FunctionSpec",
 ]
