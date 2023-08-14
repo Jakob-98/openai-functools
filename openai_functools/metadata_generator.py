37656fb3869463e5c42234635d928258172aaa3c
Jakob Serlier <37184788+Jakob-98@users.noreply.github.com>
2023-08-14 16:48:40 +0200

    [draft] Literal -> enum metadata generation (#32)
    * Add failing unit test
    
    * Add literal support by conversion to enum
    
    * Fix linter errors
    
    ---------
    
    Co-authored-by: marcvanduyn <marcvanduyn1@gmail.com>

--- openai_functools/metadata_generator.py
+++ openai_functools/metadata_generator.py
@@ -1,6 +1,7 @@
 import inspect
+import typing
 from functools import wraps
-from typing import Any, Callable
+from typing import Any, Callable, get_type_hints
 
 from docstring_parser import parse
 
@@ -9,6 +10,8 @@ from openai_functools.types import python_type_to_openapi_type
 
 def openai_function(func: Callable) -> Callable:
     func.openai_metadata = extract_openai_function_metadata(func)
+
+    print(get_type_hints(func))
 
     @wraps(func)
     def wrapper(*args: Any, **kwargs: Any) -> Any:
@@ -43,16 +46,36 @@ def extract_openai_function_metadata(func: Callable) -> dict:
     return metadata
 
 
+def extract_literal_allowed_values(func):
+    signature = inspect.signature(func)
+    literal_parameters = {}
+
+    for param_name, param in signature.parameters.items():
+
+        if isinstance(param.default, typing._LiteralGenericAlias):
+            literal_parameters[param_name] = list(param.default.__args__)
+
+    return literal_parameters
+
+
 def extract_parameter_properties(
     param: inspect.Parameter, docstring_params: dict
 ) -> dict:
     name = param.name
+
     properties = {
         "type": python_type_to_openapi_type(param.annotation)
         if param.annotation != param.empty
         else "string"  # make this configurable?
     }
+
     properties["description"] = docstring_params.get(name, name)
-    if param.default != param.empty:
-        properties["default"] = param.default
+
+    if isinstance(param.default, typing._LiteralGenericAlias):
+        properties["enum"] = list(param.default.__args__)
+        properties["default"] = param.default.__args__[0]
+    else:
+        if param.default != param.empty:
+            properties["default"] = param.default
+
     return properties
